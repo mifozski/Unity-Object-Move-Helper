1dc704867845056c99cd994c54cbf2b795c323b7
mifozski@gmail.com <mifozski@gmail.com>
2023-12-08 00:10:19 -0300

    initial

---
+++ Editor/AssetMoveHelper.cs
@@ -0,0 +1,197 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+namespace MysticEggs
+{
+	class SimpleTreeViewWindow : EditorWindow
+	{
+		[SerializeField] TreeViewState m_TreeViewState;
+
+		FolderSearcherView m_FolderSearchView;
+
+		const string SEARCH_FIELD_ID = "SearchField";
+
+		public string searchString = "";
+
+		void OnEnable()
+		{
+			if (m_TreeViewState == null)
+			{
+				m_TreeViewState = new TreeViewState();
+			}
+
+			m_FolderSearchView = new FolderSearcherView(m_TreeViewState);
+
+			m_FolderSearchView.OnSelectedFolder += HandleSelectedFolder;
+		}
+
+		private void HandleSelectedFolder(string folderPath)
+		{
+			var selectedAssets = Selection.assetGUIDs;
+
+			Debug.Log($"Moving {selectedAssets.Length} object(s) to " + folderPath);
+
+			foreach (var asset in selectedAssets)
+			{
+				var path = AssetDatabase.GUIDToAssetPath(asset);
+				var assetName = Path.GetFileName(path);
+				var error = AssetDatabase.MoveAsset(path, $"Assets/{folderPath}/{assetName}");
+				if (error)
+				{
+					Debug.LogError("Error while moving asset " + assetName + ": " + error);
+				}
+			}
+			AssetDatabase.SaveAssets();
+		}
+
+		void OnGUI()
+		{
+			GUI.SetNextControlName(SEARCH_FIELD_ID);
+			var searchInputRect = new Rect(10, 10, 200, 20);
+			searchString = GUI.TextField(new Rect(10, 10, 200, 20), searchString, 25);
+
+			// Draw Plcaeholder
+			if (string.IsNullOrEmpty(searchString)) {
+				GUIStyle style = new GUIStyle
+				{
+					alignment = TextAnchor.UpperLeft,
+					padding = new RectOffset(3, 0, 2, 0),
+					fontStyle = FontStyle.Italic,
+					normal =
+					{
+						textColor = Color.grey
+					}
+				};
+				EditorGUI.LabelField(searchInputRect, "Search Folders", style);
+			}
+
+			if (_inputNeedsFocus)
+			{
+				_inputNeedsFocus = false;
+				GUI.FocusControl(SEARCH_FIELD_ID);
+			}
+
+			var update = GUI.Button(new Rect(250, 10, 100, 20), "Move");
+			if (update)
+			{
+				m_FolderSearchView.Reload();
+			}
+
+			m_FolderSearchView.searchString = searchString;
+
+			m_FolderSearchView.OnGUI(new Rect(10, 20 + 20, position.width, position.height));
+		}
+
+		[MenuItem("Assets/Move to... %m", false)]
+		static void ShowMoveToWindow()
+		{
+			var window = GetWindow<SimpleTreeViewWindow>();
+			window.titleContent = new GUIContent("Move to folder");
+			window.Show();
+		}
+
+		bool _inputNeedsFocus = true;
+	}
+
+	public class FolderSearcherView : TreeView
+	{
+		public Action<string> OnSelectedFolder;
+		public FolderSearcherView(TreeViewState state) : base(state)
+		{
+			Reload();
+
+			rowHeight = 20;
+		}
+
+		protected override TreeViewItem BuildRoot()
+		{
+			var root = new TreeViewItem { id = 0, depth = -1, displayName = "Root" };
+
+			var allItems = new List<TreeViewItem>();
+
+			_dirPathsPerId.Clear();
+			AddSubdirectioriesToTree(Application.dataPath, allItems, "", 0);
+
+			SetupParentsAndChildrenFromDepths(root, allItems);
+
+			return root;
+		}
+
+		void AddSubdirectioriesToTree(string parentDir, List<TreeViewItem> allItems, string path, int depth)
+		{
+			string[] dirs = Directory.GetDirectories(parentDir);
+
+			foreach (var dirPath in dirs)
+			{
+				string fullPath = Path.GetFullPath(dirPath).TrimEnd(Path.DirectorySeparatorChar);
+				string dir = Path.GetFileName(fullPath);
+
+				var id = dirPath.GetHashCode();
+				var newPath = path + (string.IsNullOrEmpty(path) ? "" : "/") + dir;
+
+				_dirPathsPerId.Add(id, newPath);
+
+
+				allItems.Add(new DirItem(
+					id,
+					depth,
+					dir,
+					newPath
+				));
+
+				AddSubdirectioriesToTree(dirPath, allItems, newPath, depth + 1);
+			}
+		}
+
+		protected override void SelectionChanged(IList<int> selectedIds)
+		{
+			foreach (var id in selectedIds)
+			{
+				Debug.Log(_dirPathsPerId[id]);
+			}
+			base.SelectionChanged(selectedIds);
+		}
+
+		protected override void RowGUI(RowGUIArgs args)
+		{
+			var item = _dirPathsPerId[args.item.id];
+			if (item == null)
+			{
+				return;
+			}
+
+			var iconRect = args.rowRect;
+			iconRect.width = 16f;
+
+			var labelRect = args.rowRect;
+			var indent = iconRect.x + iconRect.width;
+			labelRect.x += indent;
+			labelRect.width -= indent;
+
+			var labelStyle = GUI.skin.label;
+			labelStyle.fontSize = 15;
+			EditorGUI.LabelField(labelRect, item, labelStyle);
+		}
+
+		protected override void DoubleClickedItem(int id)
+		{
+			OnSelectedFolder(_dirPathsPerId[id]);
+		}
+
+		static Dictionary<int, string> _dirPathsPerId = new Dictionary<int, string>();
+	}
+
+	internal class DirItem : TreeViewItem
+	{
+		public string FullPath { get; private set; }
+
+		public DirItem(int id, int depth, string displayName, string fullPath) : base(id, depth, displayName)
+		{
+			FullPath = fullPath;
+		}
+	}
+}
---
+++ Editor/HierarchyMoveHelper.cs
@@ -0,0 +1,269 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+using UnityEditor.IMGUI.Controls;
+
+namespace MysticEggs
+{
+	public static class Test
+	{	
+		[MenuItem("GameObject/Move Helper/Move Out of Parent", false, 0)]
+		static void MoveOutOfParent()
+		{
+			var gos = Selection.gameObjects;
+
+			bool changed = false;
+			foreach (var go in gos)
+			{
+				if (go.transform.parent)
+				{
+					changed = true;
+
+					if (go.transform.parent.parent)
+					{
+						go.transform.parent = go.transform.parent.parent;
+					}
+					else
+					{
+						go.transform.parent = null;
+					}
+				}
+			}
+
+			if (changed)
+			{
+				EditorSceneManager.MarkSceneDirty(gos[0].scene);
+			}
+		}
+	}
+
+	class HierarchySearchWindow : EditorWindow
+	{
+		[SerializeField] TreeViewState _treeViewState;
+
+		public string searchString = "";
+
+		const string SEARCH_FIELD_ID = "SearchField";
+
+		void OnEnable()
+		{
+			if (_treeViewState == null)
+			{
+				_treeViewState = new TreeViewState();
+			}
+
+			_objectSearchView = new SceneObjectSearcherView(_treeViewState, selectedObjects);
+
+			_objectSearchView.OnSelectedObject += HandleSelectedNewParent;
+		}
+
+		private void HandleSelectedNewParent(Transform targetParent)
+		{
+			Debug.Log($"Moving {selectedObjects.Length} object(s) to " + targetParent.name);
+
+			foreach (var obj in selectedObjects)
+			{
+				obj.transform.parent = targetParent;
+			}
+
+			Close();
+		}
+
+		void OnGUI()
+		{
+			if (Event.current.keyCode == KeyCode.Escape)
+			{
+				Close();
+			}
+
+			GUI.SetNextControlName(SEARCH_FIELD_ID);
+			var searchInputRect = new Rect(10, 10, 200, 20);
+			searchString = GUI.TextField(new Rect(10, 10, 200, 20), searchString, 25);
+
+			// Draw Plcaeholder
+			if (string.IsNullOrEmpty(searchString)) {
+				GUIStyle style = new GUIStyle
+				{
+					alignment = TextAnchor.UpperLeft,
+					padding = new RectOffset(3, 0, 2, 0),
+					fontStyle = FontStyle.Italic,
+					normal =
+					{
+						textColor = Color.grey
+					}
+				};
+				EditorGUI.LabelField(searchInputRect, "Search Scene Objects", style);
+			}
+			
+			if (_inputNeedsFocus || (Event.current.keyCode != KeyCode.None && Event.current.keyCode != KeyCode.Return && Event.current.keyCode != KeyCode.KeypadEnter && Event.current.keyCode != KeyCode.UpArrow && Event.current.keyCode != KeyCode.DownArrow))
+			{
+				_inputNeedsFocus = false;
+				GUI.FocusControl(SEARCH_FIELD_ID);
+			}
+
+			if (Event.current.keyCode == KeyCode.DownArrow)
+			{
+				if (GUI.GetNameOfFocusedControl() == SEARCH_FIELD_ID)
+				{
+					_objectSearchView.SetFocus();
+				}
+			}
+
+			_objectSearchView.searchString = searchString;
+
+			_objectSearchView.OnGUI(new Rect(10, 20 + 20, position.width, position.height));
+
+			var move = GUI.Button(new Rect(250, 10, 100, 20), "Move");
+
+			if (move || Event.current.keyCode == KeyCode.Return || Event.current.keyCode == KeyCode.KeypadEnter)
+			{
+				var selectedItems = _objectSearchView.GetSelection();
+				if (selectedItems.Count > 0)
+				{
+					var selectedTrasnform = _objectSearchView.GetSelectedTransform();
+					if (selectedTrasnform)
+					{
+						HandleSelectedNewParent(selectedTrasnform);
+					}
+				}
+			}
+		}
+
+		[MenuItem("GameObject/Move Helper/Move to... %#m", false, 0)]
+		static void MoveTo()
+		{
+			selectedObjects = Selection.gameObjects;
+
+			var window = GetWindow<HierarchySearchWindow>();
+			window.titleContent = new GUIContent("Move to object");
+			window.Show();
+		}
+
+		bool _inputNeedsFocus = true;
+
+		static GameObject[] selectedObjects;
+
+		SceneObjectSearcherView _objectSearchView;
+	}
+
+	public class SceneObjectSearcherView : TreeView
+	{
+		public Action<Transform> OnSelectedObject;
+
+		public SceneObjectSearcherView(TreeViewState state, GameObject[] objectsToSkip) : base(state)
+		{
+			_objectsToSkip = objectsToSkip.ToDictionary(obj => obj.transform, (obj) => true);
+
+			Reload();
+
+			rowHeight = 20;
+		}
+
+		public Transform GetSelectedTransform()
+		{
+			var selectedItems = GetSelection();
+			if (selectedItems.Count > 0)
+			{
+				return _transformsPerId[selectedItems.First()];
+			}
+			return null;
+		}
+
+		protected override TreeViewItem BuildRoot()
+		{
+			var root = new TreeViewItem { id = 0, depth = -1, displayName = "Root" };
+
+			var allItems = new List<TreeViewItem>();
+
+			_transformsPerId.Clear();
+
+			var rootObjects = SceneManager.GetActiveScene().GetRootGameObjects();
+
+			foreach (var rootObj in rootObjects)
+			{
+				AddChildObjectsToTree(rootObj.transform, allItems, "", 0);
+			}
+
+			SetupParentsAndChildrenFromDepths(root, allItems);
+
+			return root;
+		}
+
+		void AddChildObjectsToTree(Transform obj, List<TreeViewItem> allItems, string path, int depth)
+		{
+			if (_objectsToSkip.ContainsKey(obj))
+			{
+				return;
+			}
+
+			var newPath = path + (string.IsNullOrEmpty(path) ? "" : "/") + obj.name;
+			var id = obj.GetInstanceID().GetHashCode();
+			allItems.Add(new GameObjectItem(
+				id,
+				depth,
+				newPath,
+				newPath
+			));
+
+			_transformsPerId.Add(id, obj);
+
+			foreach (Transform child in obj)
+			{
+				AddChildObjectsToTree(child, allItems, newPath, depth + 1);
+			}
+		}
+
+		protected override void SelectionChanged(IList<int> selectedIds)
+		{
+			base.SelectionChanged(selectedIds);
+		}
+
+		protected override void RowGUI(RowGUIArgs args)
+		{
+			var item = _transformsPerId[args.item.id];
+			if (item == null)
+			{
+				return;
+			}
+
+			var labelRect = args.rowRect;
+
+			var labelStyle = GUI.skin.label;
+			labelStyle.fontSize = 15;
+			EditorGUI.LabelField(labelRect, args.item.displayName, labelStyle);
+		}
+
+		protected override bool DoesItemMatchSearch(TreeViewItem item, string search)
+		{
+			return item.displayName.IndexOf(search, StringComparison.OrdinalIgnoreCase) >= 0;
+		}
+
+		protected override bool CanMultiSelect(TreeViewItem item)
+		{
+			return false;
+		}
+
+		protected override void DoubleClickedItem(int id)
+		{
+			OnSelectedObject(_transformsPerId[id]);
+		}
+
+		static Dictionary<int, Transform> _transformsPerId = new Dictionary<int, Transform>();
+
+		Dictionary<Transform, bool> _objectsToSkip;
+	}
+
+	internal class GameObjectItem : TreeViewItem
+	{
+		public string FullPath { get; private set; }
+
+		public GameObjectItem(int id, int depth, string displayName, string fullPath) : base(id, depth, displayName)
+		{
+			FullPath = fullPath;
+		}
+	}
+}
